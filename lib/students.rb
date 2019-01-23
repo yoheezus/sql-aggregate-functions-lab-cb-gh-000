f95a6d88ae46a8755407ec9cf3cd79abda1785d1
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-23 13:56:51 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -17,5 +17,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-    "SELECT grade, AVG(gpa) FROM students; GROUP BY country;"
+    "SELECT grade, AVG(gpa) FROM students; GROUP BY grade;"
 end
